a4e1106a9599df4d30440d798c2485282ee70793
rennaarenata <59098319+rennaarenata@users.noreply.github.com>
2022-04-21 14:00:13 +0200

    stuff :1

--- basic-viewer.py
+++ basic-viewer.py
@@ -1,5 +1,4 @@
 # ----------------------- # Imports
-import requests
 import argparse
 # ----------------------- # Froms
 from selenium import webdriver
@@ -24,8 +23,8 @@ def argscheck(v, d, s):
 parser = argparse.ArgumentParser(description="Youtube-AutoViewer by Rennaarenata (using this tool you may encounter a permanent ban from YouTube)")
 parser.add_argument("url", help="The URL of the video.")
 parser.add_argument("-v", "--views", help="The approximate number of views you want to get (default 40), do not exceed 100 views in one hour.", default=40)
-parser.add_argument("-d","--delay", help="Every how many requests stop the bot.", default=20)
-parser.add_argument("-s","--seconds", help="How many seconds the bot has wait every {-d} request(s)", default=15)
+parser.add_argument("-d","--delay", help="Every how many requests stop the bot. - default: 20", default=20)
+parser.add_argument("-s","--seconds", help="How many seconds the bot has wait every {-d} request(s) - default: 15", default=15)
 
 args = parser.parse_args()
 url = args.url
@@ -33,22 +32,34 @@ views = args.views
 delay = args.delay
 seconds = args.seconds
 
+print("[+]Starting...")
+print("[i]Views: " + str(views))
+print("[i]Delay: " + str(delay))
+print("[i]Seconds: " + str(seconds))
+if (int(views) >= 100 and int(seconds) < 15):
+	print("[!!]Warning: You are trying to get more than 100 views, this may result in a permanent ban from YouTube. Please consider lowering the number of views or increasing the seconds (-s).")
+	sleep(10)
+
+
 # ------------ # Check
 argscheck(views, delay, seconds)
 
-# ------------ # Driver Things
-driver1 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVERE")
-driver2 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVERE")
-driver3 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVERE")
-driver4 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVERE")
+options = webdriver.ChromeOptions()
+options.add_argument("--log-level=3")
+
+# ------------ # Driver Stuff
+driver1 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVER.EXE", service_log_path="chromedriver.log", options=options)
+driver2 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVER.EXE", service_log_path="chromedriver.log", options=options)
+driver3 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVER.EXE", service_log_path="chromedriver.log", options=options)
+driver4 = webdriver.Chrome(executable_path="PATHTOCHROMEDRIVER.EXE", service_log_path="chromedriver.log", options=options)
+print("[*]Sleeping for 6 seconds...Accept cookies in the meantime.")
 
 driver1.get(url)
 driver2.get(url)
 driver3.get(url)
 driver4.get(url)
-
-print("Sleeping for 6 seconds...")
 sleep(6)
+
 
 for a in range(int(views) + 1):
 	driver1.refresh()
@@ -59,4 +70,9 @@ for a in range(int(views) + 1):
 	if(a % int(delay) == 0):
 		print(f"Sleeping for {seconds} seconds")
 		sleep(int(seconds))
+
 print(f"Enjoy!\n")
+driver1.close()
+driver2.close()
+driver3.close()
+driver4.close()
